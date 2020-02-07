82817db482903729e6d68a0d670cf2e6a41b68ab
Dmitry Sheremet <dmi.sheremet@gmail.com>
2020-02-07 23:09:30 +0300

    1 hour

---
+++ Assets/Scripts/NeighborMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeighborMovement : MonoBehaviour
+{
+    [SerializeField] private float StartSpeed = 3;
+    [SerializeField] private Transform MinPosPoint;
+    [SerializeField] private Transform MaxPosPoint;
+    private Rigidbody2D rb;
+    private float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        speed = StartSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        var dir = new Vector2(transform.localScale.x, 0);
+        var vel = dir * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + vel);
+
+        if ((rb.position.x < MinPosPoint.position.x && dir.x < 0)
+            || (rb.position.x > MaxPosPoint.position.x && dir.x > 0))
+        {
+            TurnBack();
+        }
+    }
+
+    private void TurnBack()
+    {
+        var scale = transform.localScale;
+        scale.x = -scale.x;
+        transform.localScale = scale;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D Target;
+    [SerializeField] private Transform ForceApplyPoint;
+    [SerializeField] private float Power = 10;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Target != null)
+        {
+            Debug.Log("APPLY");
+            //var dir = Target.position - (Vector2)transform.position;
+            var deltaX = Target.position.x - ForceApplyPoint.position.x;
+            var dir = new Vector2(deltaX, 0);
+            var force = dir.normalized * Power;
+            var pos = (Vector2)transform.position;
+            Target.gravityScale = 1;
+            Target.AddForceAtPosition(force, pos, ForceMode2D.Impulse);
+            Target = null;
+        }
+    }
+}
